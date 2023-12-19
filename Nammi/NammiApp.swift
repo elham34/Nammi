f28ae98b651bbcce233573418036ff1735f4f686
elham34 <elham.alwadie@gmail.com>
2023-12-19 10:48:07 +0300

    Initial Commit

---
+++ Nammi/NammiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NammiApp.swift
+//  Nammi
+//
+//  Created by Elham on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct NammiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
