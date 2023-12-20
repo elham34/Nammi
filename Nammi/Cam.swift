de87bfbd1348f91d410884a7ad7d8fffdfd0506d
GhadahAlb <ghadah.b13@gmail.com>
2023-12-20 10:55:08 +0300

    Add Splash

---
+++ Nammi/Cam.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Nammi
+//
+//  Created by Ghadah Albassam on 19/12/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ Nammi/Intro.swift
@@ -0,0 +1,18 @@
+//
+//  Intro.swift
+//  Nammi
+//
+//  Created by Ghadah Albassam on 19/12/2023.
+//
+
+import SwiftUI
+
+struct Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Intro()
+}
